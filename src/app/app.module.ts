151f6c10fddc56845d425a70b4dbabc16afda8be
prat-gupta <pratikshagupta789@gmail.com>
2019-09-22 15:17:13 +0530

    prat is smart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { MenuComponentComponent } from './menu-component/menu-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    MenuComponentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    RouterModule.forRoot(
+      [
+        { path: '', redirectTo: '/header', pathMatch: 'full' },
+       { path: 'header', component: HeaderComponent }
+
+      ]
+    )
+
   ],
   providers: [],
   bootstrap: [AppComponent]

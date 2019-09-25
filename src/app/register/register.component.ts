2d04b25d37645ba03b842560efbd3a8f3a568713
dhawalshah48 <dhawalshah48@gmail.com>
2019-09-25 12:13:56 +0530

    same

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http'
+import 'rxjs/add/operator/map'
+import { Subscriber } from '../../../node_modules/rxjs';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  d={"r":null}
+  constructor(private http: Http) { }
 
   ngOnInit() {
+    this.http.get("http://jsonplaceholder.typicode.com/users").map((res) => res.json()).subscribe((data) => console.log(data))
+  }
+
+  onClickSubmit(data){
+    this.http.post("http://localhost:3000/webapi/insertdata",data).map((res) => res.json()).subscribe((data) => this.showresponse(data))
+  }
+
+  showresponse(data){
+    console.log(data)
+    this.d=data
   }
 
 }
